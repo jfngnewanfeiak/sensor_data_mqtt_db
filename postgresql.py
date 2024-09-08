242b92e7d4006fb85984b73966a02512d9449986
OK220259\taishin <ok220259@oks.iput.ac.jp>
2024-09-08 23:44:22 +0900

    関数にコメントを付与

--- postgresql.py
+++ postgresql.py
@@ -1,6 +1,10 @@
 import psycopg2
 import os
 class POSTGRESQL:
+    """
+    Note:
+        ポスグレに接続できるpsycopg2を自分なりにラッパーしたやつ
+    """
     def __init__(self) -> None:
         self._host = ''
         self._user = ''
@@ -10,12 +14,23 @@ class POSTGRESQL:
     
 
     def setting_connection(self,host:str,user:str,password:str,database:str):
+        """
+        Args:
+            host : ipアドレス
+            user : 接続するポスグレのユーザーネーム
+            password : 接続するポスグレのパスワード
+            database : 接続するDBの名前
+        """
         self._host = host
         self._user = user
         self._password = password
         self._database = database
     
     def connect_DB(self):
+        """
+        Note : ポスグレに接続するやつ setting_connectionの後に実行,
+               connect DB 〇〇と出ればOK
+        """
         self._db_connection = psycopg2.connect(
             host=self._host,
             user=self._user,
@@ -25,6 +40,12 @@ class POSTGRESQL:
         print("connect DB {}".format(self._database))
     
     def exec_update(self,query:str):
+        """
+        Args:
+            query : クエリ文をそのままいれる(str)
+        Note: 
+            元あるデータを更新する用の実行関数(insertとかupdateとか)。戻り値なし.
+        """
         with self._db_connection:
             with self._db_connection.cursor() as cursor:
                 cursor.execute(query=query)
@@ -32,6 +53,12 @@ class POSTGRESQL:
         cursor.close()
     
     def exec_select(self,query:str):
+        """
+        Args:
+            query : クエリ文をそのままいれる(str)
+        Note: 
+            名前の通りselectする用のやつ。戻り値あり、タプル型で返ってくる
+        """
         retV = None
         with self._db_connection:
             with self._db_connection.cursor() as cursor:
